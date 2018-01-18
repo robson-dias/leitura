80a20a63185b882a4b10022908c1855f215747b4
Robson Dias <robson@binariio.com>
2018-01-17 23:50:23 -0200

    ajustes

--- frontend/src/Components/PostList.js
+++ frontend/src/Components/PostList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Post from './Post'
+import { Alert, Button } from 'react-bootstrap'
 
 class PostList extends Component {
     componentDidMount() {
@@ -12,16 +13,21 @@ class PostList extends Component {
 
         return (
             <div>
-                {posts.map((post) => 
-                    <Post 
-                        key={post.id}
-                        title={`${post.title}`}
-                        body={post.body}
-                        category={post.category}
-                        author={post.author}
-                        footer={`Rodape`}
-                    />
-                )}
+                {posts.length > 0 ? 
+                    posts.map((post) => 
+                        <Post 
+                            key={post.id}
+                            title={`${post.title}`}
+                            body={post.body}
+                            category={post.category}
+                            author={post.author}
+                            footer={`Rodape`}
+                        />
+                    ) : 
+                    <Alert bsStyle="warning">
+                        <strong>Nenhum post encontrado!</strong> Clique em <b>New Post</b> para adicionar uma nova postagem.
+                    </Alert>
+                }
             </div>
         )
     }
