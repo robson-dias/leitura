cc468cde67720332703780383cf6149d02d52c38
Robson Dias <robson@binariio.com>
2018-01-26 14:16:52 -0200

    feat: logger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,19 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
-import reducer from './Reducers'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
+import reducer from './Reducers'
+import logger from 'redux-logger'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(logger)
+    )
 )
 
 ReactDOM.render(
